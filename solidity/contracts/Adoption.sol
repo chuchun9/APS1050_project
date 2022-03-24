20cbdf4cd4ab03ac2973636d94189167883228a4
chun tung chu <chuntungchu@chuns-MacBook-Pro-2.local>
2022-03-24 16:46:26 -0400

    first commity

--- solidity/contracts/Adoption.sol
+++ solidity/contracts/Adoption.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.5.0;
 contract Adoption {
     address[16] public adopters;
 
-
     // Adopting a pet
     function adopt(uint petId) public returns (uint) {
         require(petId >= 0 && petId <= 15);
 
         adopters[petId] = msg.sender;
 
+        
         return petId;
     }
 
